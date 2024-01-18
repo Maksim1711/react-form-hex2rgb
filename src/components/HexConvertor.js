079ae361d6a25b8771112bd6b9d98ab649a11876
Maksim Oloviannikov <olovjanqnet@gmail.com>
2024-01-18 15:46:58 +0300

    work added2

--- src/components/HexConvertor.js
+++ src/components/HexConvertor.js
@@ -1,29 +1,29 @@
 import { useState } from 'react';
 
 export default function HexConvertor() {
-  const [color, setColor] = useState('');
+	const [color, setColor] = useState('');
 
-  const handleChange = ({ target }) => {
-    if (target.value.length < 7) {
-      return;
-    }
+	const handleChange = ({ target }) => {
+		if (target.value.length < 7) {
+			return;
+		}
 
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(target.value);
-    if (result === null) {
-      setColor('Ошибка!');
-      return;
-    }
+		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(target.value);
+		if (result === null) {
+			setColor('Ошибка!');
+			return;
+		}
 
-    const rgb = `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
-    setColor(`rgb(${rgb})`);
-  }
+		const rgb = `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
+		setColor(`rgb(${rgb})`);
+	}
 
-  return (
-    <div className="HexConvertor" style={{ backgroundColor: color }}>
-      <div className="HexConvertor__container">
-        <input className="HexConvertor__input" onChange={handleChange} />
-        <div className="HexConvertor__result">{color}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="HexConvertor" style={{ backgroundColor: color }}>
+			<div className="HexConvertor__container">
+				<input className="HexConvertor__input" onChange={handleChange} maxlength="7" />
+				<div className="HexConvertor__result">{color}</div>
+			</div>
+		</div>
+	);
 }
